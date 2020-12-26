7a8cd3e8671ad251c955f0a8d05ddf9ce9f86507
G-K-R-git <germes198990@gmail.com>
2020-12-26 13:06:27 +0300

    Бинарный поиск

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,5 +9,29 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
+    if (len(arr) == 0) or (len(arr) == 1 and arr[0] != elem) or (elem < arr[0]) or (elem > arr[len(arr)-1]):
+        return None
+    elif len(arr) == 1 and arr[0] == elem:
+        return 0
+
+    part = arr
+    result = 0
+    while len(part) > 2:
+        ind = len(part) // 2
+        val = part[ind]
+        if elem < val:
+            part = part[:ind]
+        elif elem > val:
+            part = part[ind:]
+            result += ind
+        else:
+            part = part[:ind + 1]
+        if len(part) == 2 and part[1] == elem:
+            return result+1
+        elif len(part) <= 2 and part[0] == elem:
+            return result
+        elif len(part) <= 2 and part[0] != elem:
+            return None
+
     print(elem, arr)
-    return None
+    return result
