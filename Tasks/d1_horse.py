918b5bedc18bb78d424c0913755224e62ec4a815
G-K-R-git <germes198990@gmail.com>
2021-01-29 17:22:08 +0300

    d1 Конь (недоделанный, проблемы)

--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -1,3 +1,6 @@
+from tabulate import tabulate
+
+
 def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     """
     Given field with size rows*cols count available paths from (0, 0) to point
@@ -8,34 +11,46 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     """
     paths_cost = [[0 for _ in range(shape[0])] for _ in range(shape[1])]
     paths_cost[0][0] = 1
-    # print(paths_cost)
-    turns = [(-2, 1), (2, 1), (-1, 2), (1, 2)]
-    height = 0
+    turns = [(-2, 1), (2, 1), (-1, 2), (1, 2)]  # Массив возможных ходов (исключает ходы "вниз")
+    height = 0  # Текущий "уровень"
     positions = [(0, 0)]
     new_pos = []
-    while height != point[1]:
+    while height < point[1]:
+        # print("Height", height)
         for position in positions:
+            # print("Current position", position)
             x = position[0]
             y = position[1]
             for turn in turns:
+                # print("Current turn", turn)
                 x += turn[0]
                 y += turn[1]
-                if x < 0 or x > shape[0]-1 or y > shape[1]-1:
-                    x -= turn[0]
-                    y -= turn[1]
-                    pass
-                else:
+                # print("One of the next positions for current pos", x, y)
+                if 0 <= x <= shape[0]-1 and y <= shape[1]-1:
                     new_pos.append((x, y))
-                    print(x, y, position, positions, height)
                     paths_cost[y][x] += 2*paths_cost[position[1]][position[0]]
                     x -= turn[0]
                     y -= turn[1]
+                else:
+                    x -= turn[0]
+                    y -= turn[1]
         positions = set(new_pos)
+        # print("Next positions", positions)
         new_pos = []
         height += 1
+    print("Paths cost ", plot_costs(paths_cost))
     return paths_cost[point[1]][point[0]]
+
+
+def plot_costs(field):
+    to_show = []
+    for i in range(1, len(field)+1):
+        to_show.append(field[-i])
+    print(tabulate(to_show, tablefmt="grid", showindex=[j for j in range(len(field), 0, -1)]))
 
 
 if __name__ == '__main__':
     # shape = (8, 8)
     print('Result ', calculate_paths((9, 9), (8, 8)))
+    print('Result ', calculate_paths((8, 8), (7, 7)))
+    print("Result ", calculate_paths((17, 12), (16, 9)))
