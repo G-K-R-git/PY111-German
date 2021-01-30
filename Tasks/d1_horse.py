84ff5f1d4bcf868007154c482cd6a09051749a9b
G-K-R-git <germes198990@gmail.com>
2021-01-30 03:04:14 +0300

    d1 Конь доделанный

--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -9,37 +9,31 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     :param point: desired point for horse
     :return: count of paths from (1, 1) to (point[0], point[1]) (numerating from 0, so (0, 0) - left bottom tile)
     """
-    paths_cost = [[0 for _ in range(shape[0])] for _ in range(shape[1])]
+    paths_cost = [[0 for _ in range(shape[1])] for _ in range(shape[0])]
     paths_cost[0][0] = 1
     turns = [(-2, 1), (2, 1), (-1, 2), (1, 2)]  # Массив возможных ходов (исключает ходы "вниз")
     height = 0  # Текущий "уровень"
     positions = [(0, 0)]
     new_pos = []
-    while height < point[1]:
-        # print("Height", height)
+    while height < point[0]:
         for position in positions:
-            # print("Current position", position)
-            x = position[0]
-            y = position[1]
+            x, y = position
             for turn in turns:
-                # print("Current turn", turn)
                 x += turn[0]
                 y += turn[1]
-                # print("One of the next positions for current pos", x, y)
-                if 0 <= x <= shape[0]-1 and y <= shape[1]-1:
-                    new_pos.append((x, y))
+                if 0 <= x <= shape[1]-1 and y <= shape[0]-1:
                     paths_cost[y][x] += 2*paths_cost[position[1]][position[0]]
-                    x -= turn[0]
-                    y -= turn[1]
+                    x, y = position
                 else:
-                    x -= turn[0]
-                    y -= turn[1]
+                    x, y = position
+        for pos in range(shape[1]):
+            if paths_cost[height+1][pos]:
+                new_pos.append((pos, height+1))
         positions = set(new_pos)
-        # print("Next positions", positions)
         new_pos = []
         height += 1
-    print("Paths cost ", plot_costs(paths_cost))
-    return paths_cost[point[1]][point[0]]
+    plot_costs(paths_cost)
+    return paths_cost[point[0]][point[1]]
 
 
 def plot_costs(field):
@@ -50,7 +44,7 @@ def plot_costs(field):
 
 
 if __name__ == '__main__':
-    # shape = (8, 8)
-    print('Result ', calculate_paths((9, 9), (8, 8)))
     print('Result ', calculate_paths((8, 8), (7, 7)))
+    print('Result ', calculate_paths((9, 9), (8, 8)))
     print("Result ", calculate_paths((17, 12), (16, 9)))
+    print("Result ", calculate_paths((12, 10), (11, 9)))
